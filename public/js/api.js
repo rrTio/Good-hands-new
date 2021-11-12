50d2aa1a25dfdc18f14cc7b7917cf8d4180f4db7
rrTio <tio.rozsraven@ue.edu.ph>
2021-11-13 02:32:39 +0800

    First commit

---
+++ public/js/api.js
@@ -0,0 +1,5 @@
+
+function booking() {
+    var bookingId = ["tracking"]["trackingCode"];
+    window.location.href = "https://api-test.goodhands.ph/api/GetBookingById?BookingId=";
+}
---
+++ public/js/booking.js
@@ -0,0 +1,5 @@
+function makeDivAppear(){
+    var container = document.getElementById("tracker");
+    container.classList.remove("hidden");
+    window.location.href = "#tracker"
+}
